ecd5d2183ae2a2ccc098ce4839a12a8cf5899653
Erik Fakin <e.fakin3@gmail.com>
2024-10-03 10:27:28 +0200

    selettori

---
+++ selettori/script.js
@@ -0,0 +1,18 @@
+// Selettore con l'id
+let titolo = document.getElementById('titolo');
+titolo.innerText = 'Titolo da JS';
+titolo.style.color = '#ffbbcc';
+
+// Possiamo utilizzare i selettori css
+
+// document.querySelector mi ritorna solo un elemento, 
+// il primo che trova
+const primoElemento = document.querySelector('.elemento');
+console.log("Primo elemento",primoElemento);
+primoElemento.innerText = "Primo elemento";
+
+// document.querySelectorAll mi ritorna la lista di 
+// tutti gli elementi
+const elementi = document.querySelectorAll('.elemento');
+console.log("Lista di elementi", elementi);
+
