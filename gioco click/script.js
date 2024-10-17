f702b56540f0c3113cbce5a414a08fd83d7951a3
Erik Fakin <e.fakin3@gmail.com>
2024-10-17 11:05:48 +0200

    cps

--- gioco click/script.js
+++ gioco click/script.js
@@ -1,14 +1,43 @@
 const pulsanteStart = document.querySelector('.start');
 const timer = document.querySelector('.timer');
+const contenitore = document.querySelector('.contenitore');
+const total = document.querySelector('.total');
+const cps = document.querySelector('.cps');
+
+let startTime;
+let idInterval;
+let clickTotali = 0;
+let time = 0;
+
+let hasStarted = false;
 
 function onStartClick(){
     pulsanteStart.style.display = "none";
+    hasStarted = true;
+    time = 0;
+    clickTotali = 0;
+    startTime = Date.now();
+    idInterval = setInterval(onTimerUpdate, 1000/60);
 }
 
 pulsanteStart.addEventListener('click', onStartClick)
 
 function onTimerUpdate(){
-    
+    time = Date.now() - startTime
+    timer.innerHTML = "Tempo trascorso: " + time / 1000; 
+    total.innerHTML = "Click totali: " + clickTotali;
+    cps.innerHTML = "CPS: " + clickTotali / (time / 1000);
+    if(time >= 5000){
+        clearInterval(idInterval);
+        hasStarted = false;
+    }
 }
-// Usero setInterval per fare il timer
-setInterval(onTimerUpdate, 1000/60);
+
+function onContenitoreClick(){
+    if(hasStarted == true){
+        clickTotali = clickTotali + 1;
+    }
+}
+
+contenitore.addEventListener('click', onContenitoreClick)
+
