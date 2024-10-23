ac727a203cc65248365afff105408d4d32b1d7bd
Erik Fakin <e.fakin3@gmail.com>
2024-10-23 12:21:20 +0200

    esercizi 1 e 2

---
+++ esercizi/1/script.js
@@ -0,0 +1,8 @@
+const a = prompt("Inserisci un numero: ");
+const b = prompt("Inserisci un altro numero: ");
+
+if(a==b){
+    alert("UGUALI");
+} else {
+    alert("DIVERSI");
+}
---
+++ esercizi/2/script.js
@@ -0,0 +1,9 @@
+const a = prompt("Inserisci un lato: ");
+const b = prompt("Inserisci il secondo lato");
+
+const area = a*b;
+const perimetro = 2*a + 2*b;
+
+alert("Perimentro=" + perimetro);
+alert("Area=" + area);
+
