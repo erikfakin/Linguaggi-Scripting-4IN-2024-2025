787a4ffb55854fa868431ebb0718da17bf82504d
Erik Fakin <e.fakin3@gmail.com>
2024-11-06 12:20:35 +0100

    math

---
+++ Math/script.js
@@ -0,0 +1,27 @@
+// COSTANTI
+console.log("Pi greco:", Math.PI);
+console.log("Costante di Eulero:", Math.E);
+console.log("Radice di 2:", Math.SQRT2);
+
+// METODI
+
+// Arrotondamento
+console.log("3.6 arrotondato:", Math.round(3.6));
+
+// Arrotondamento per diffetto (al numero minore)
+console.log("7.7 arrotondato per diffetto:",Math.floor(7.7));
+
+// Arrotondamento per eccesso (al numero maggiore)
+console.log("2.1 arrotondato per eccesso:", Math.ceil(2.1));
+
+// Seno e coseno
+console.log("sin(45 gradi)", Math.sin(Math.PI / 4));
+console.log("cos(60 gradi)", Math.cos(Math.PI / 3));
+
+// Valore assoluto
+console.log("Valore assouluto di -5:", Math.abs(-5));
+
+// Potenze e radici
+console.log("10 alla potenza di 5", Math.pow(10, 5));
+
+console.log("Radice quadrata di 50", Math.sqrt(50));
