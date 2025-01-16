c9b967ac65959cf5e837cce766cb19cb011de07f
Erik Fakin <e.fakin3@gmail.com>
2025-01-16 09:43:43 +0100

    contatto

---
+++ php/esercizio contatto/contatto.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="it">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Contattaci</title>
+</head>
+<body>
+    <h1>Inviaci un messaggio</h1>
+    <form>
+        <label>Nome: </label>
+        <input type="text" name="nome">
+        <br>
+        <label>Email: </label>
+        <input type="email" name="email">
+        <br>
+        <label>Telefono: </label>
+        <input type="tel" name="telefono">
+        <br>
+        <label>Messaggio: </label>
+        <textarea name="messaggio"></textarea>
+        <br>
+        <button>Invia</button>
+    </form>
+    
+</body>
+</html>
