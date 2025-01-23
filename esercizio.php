ecf81a3d30031af284e2c9cea9c1a76b47cd4086
Erik Fakin <e.fakin3@gmail.com>
2025-01-23 09:51:22 +0100

    esercizio interrogazione

--- esercizio.php
+++
@@ -1,8 +0,0 @@
-<?php
-// Esercizio: importa un file chiamato test.php
-// nel file definisci una funzione che si chiama saluta
-// e riceve un argomento $nome, che scrive sullo schermo
-// "Ciao $nome"
-// in questo file poi chiama la funzione dal modulo 
-// con il tuo nome
-
---
+++ php/interrogazione/index.php
@@ -0,0 +1,20 @@
+<?php
+# Esercizio
+# Crea una forma avente i seguienti input: 
+#   nome
+#   cognome
+#   email
+#   indirizzo
+#   un checkbox che chiede "Registrati?"
+#   pulsante per inviare la forma
+
+# La forma manda i dati con il metodo post a un file chiamato
+# elabora-forma.php . Questo file salva i dati della forma 
+# nelle variabili:
+#   $nome
+#   $cognome
+#   $email
+#   $indirizzo
+#   $registrare
+# E poi li tutti scrive sullo schermo
+
